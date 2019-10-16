3c860ff82a7b67ee6f87effacf8912215be9acc6
anwars993 <56355710+anwars993@users.noreply.github.com>
2019-10-16 03:15:19 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,108 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "anwar"
+var adminprefix = 'anwar'
+
+
+//bc
+
+client.on("message", message => {
+    if (message.content.startsWith("anwarobc")) {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+  m.send(`${argresult}\n ${m}`);
+  })
+  message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\`:mailbox:  عدد المستلمين `);
+  message.delete();
+  };
+  });
+
+
+//bc online
+
+
+  var prefix = "anwar";
+
+  client.on("message", message => {
+  
+              if (message.content.startsWith(prefix + "bc")) {
+                           if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+    let args = message.content.split(" ").slice(1);
+    var argresult = args.join(' '); 
+    message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+   m.send(`${argresult}\n ${m}`);
+  })
+   message.channel.send(`anwar`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` :mailbox:  عدد المستلمين `); 
+   message.delete(); 
+  };     
+  });
+
+client.on('message', message => {
+    var  user = message.mentions.users.first() || message.author;
+if (message.content.startsWith("anwaravatar")) {
+message.channel.send(`This avatar For ${user} link : ${user.avatarURL}`);
+}
+});
+
+client.on('ready',  () => {
+    console.log('تم تشغيل :Broadcast  ');
+    console.log(`Logged in as * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
+    console.log(`Logged in as * [ " ${client.user.username} " ] Users! [ " ${client.users.size} " ]`);
+    console.log(`Logged in as * [ " ${client.user.username} " ] channels! [ " ${client.channels.size} " ]`);
+  });
+
+  client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('/adminbc')){
+if(!message.author.id === '476185102922285066') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
+  client.on('message', msg => {
+    if(msg.content === 'anwarhelp')
+    msg.reply('Check Your DM :white_check_mark:')
+  });
+  
+  
+  client.on("message", message => {
+    if (message.content === "/help") {
+     const embed = new Discord.RichEmbed() 
+         .setColor("#00FF00")
+         .setThumbnail(message.author.avatarURL)
+         .setDescription(`**Help|هيلب
+
+       anwarobc | لأرسال برود كاست للكل
+
+       anwarbc  |  لأرسال برود كاست للأونلاين
+
+       anwaradminbc | برودكاست عادي
+       
+       anwaravatar  |تنزيل صوره ملفك او صوره ملف غيرك|
+       
+       join server : https://discord.gg/rtZ9YHu  
+       ** `)
+   message.author.sendEmbed(embed)
+   
+   }
+   });
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`join server :https://discord.gg/rtZ9YHu `,"https://discordapp.com/channels/@me/626451542794567715")
+client.user.setStatus("dnd")
+});
+
+
+client.login(process.env.BOT_TOKEN);
